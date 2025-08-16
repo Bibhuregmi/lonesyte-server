c3fe9b4fc94fcae28d05efb069fe60568bf4a7f8
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-08-15 22:41:09 -0400

    feat: Added metadata for travel-vlogs and video compression logic for ffmpeg

--- server/utils/videoMetadata.ts
+++ server/utils/videoMetadata.ts
@@ -7,3 +7,13 @@ export interface VideoMetadata{
     created_at: string;
     language?: string | null; // optional, if language metadata is needed
 }
+
+export interface VlogMetadata{
+    id?: string;
+    user_id?: string;
+    title: string | null;
+    video_url: string;
+    created_at: string;
+    latitude: number;
+    longitude: number;
+}
