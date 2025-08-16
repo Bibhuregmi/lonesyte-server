263f18c730b20144272734c708313903d3f4426c
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-08-15 22:44:57 -0400

    feat: Added new route and improved error handling

--- server/routes/videoRoutes.ts
+++ server/routes/videoRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { uploadVideo, getVideo } from "../controllers/videoControllers";
-import { authMiddleware, upload } from "../middlewares/middleware";
+import { uploadVideo, getVideo, uploadVlog } from "../controllers/videoControllers";
+import { authMiddleware, handleUploadError, upload } from "../middlewares/middleware";
 const router = Router(); 
 
-router.post('/upload', authMiddleware, upload.single("video") ,uploadVideo )
+router.post('/upload', authMiddleware, upload.single("video"), handleUploadError, uploadVideo)
+router.post('/upload-vlogs', authMiddleware, upload.single("video"), handleUploadError, uploadVlog)
 router.get('/get-video', authMiddleware, getVideo)
 
 export default router; 
